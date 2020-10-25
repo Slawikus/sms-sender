b03e39b807732ed79f8ec94abcbfeb514d79cc94
Slawa Gladkov <Slawikus@users.noreply.github.com>
2020-10-25 22:51:03 +0100

    Add Application class

--- lib/application.rb
+++ lib/application.rb
@@ -1,5 +1,22 @@
+require 'sms_splitter'
+require 'sms_carrier'
+
 class Application
+  MESSAGE_TO_SEND = 'Alice was beginning to get very tired of sitting by her '\
+      'sister on the bank, and of having nothing to do: once or twice she'\
+      ' had peeped into the book her sister was reading, but it had no '\
+      'pictures or conversations in it, “and what is the use of a book,”'\
+      ' thought Alice “without pictures or conversations?”So she was '\
+      'considering in her own mind (as well as she could, for the hot day'\
+      ' made her feel very sleepy and stupid), whether the pleasure of'\
+      ' making a daisy-chain would be worth the trouble of getting up and'\
+      ' picking the daisies, when suddenly a White Rabbit with pink eyes'\
+      ' ran close by her.'.freeze
+
   def run
-    'Running the app'
+    SmsSplitter.call(MESSAGE_TO_SEND).each do |message|
+      SmsCarrier.deliver_message(message)
+    end
+    0
   end
 end
--- test/application_test.rb
+++ test/application_test.rb
@@ -3,10 +3,31 @@ require 'application'
 
 describe Application do
   let(:application) { Application.new }
+  let(:message_1) { 'message1' }
+  let(:message_2) { 'message2' }
+  let(:splitted_messages) { [message_1, message_2] }
+
+  before do
+    allow(SmsSplitter).to receive(:call).and_return(splitted_messages)
+    allow(SmsCarrier).to receive(:deliver_message)
+  end
 
   describe '#run' do
-    it 'returns the text message' do
-      assert_equal 'Running the app', application.run
+    it 'calls SMS Splitter with the given message' do
+      expect(SmsSplitter).to receive(:call).with(Application::MESSAGE_TO_SEND)
+
+      application.run
+    end
+
+    it 'delivers splitted messages over SMS Carrier' do
+      expect(SmsCarrier).to receive(:deliver_message).with(message_1)
+      expect(SmsCarrier).to receive(:deliver_message).with(message_2)
+
+      application.run
+    end
+
+    it 'returns zero' do
+      assert_equal 0, application.run
     end
   end
 end
