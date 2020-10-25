82acb762e4f873ebfe139a47364685f4837e278b
Slawa Gladkov <Slawikus@users.noreply.github.com>
2020-10-25 23:17:36 +0100

    Refactor SmsSplitter

--- lib/sms_splitter.rb
+++ lib/sms_splitter.rb
@@ -1,18 +1,39 @@
 require 'suffixer'
 
 class SmsSplitter
-  def self.call(message)
-    return [message] if message.length <= 160
+  MAX_SMS_LENGTH = 160
+
+  def initialize(message)
+    @message = message
+  end
+
+  def call
+    return [message] if message_fits_into_one_sms?
 
     (2..).each do |total_parts|
       message_copy = message.dup
+      Suffixer.new(total_parts)
+              .map { |suffix| extract_message.call(suffix, message_copy) }
+              .tap { |splits| return splits if message_copy.empty? }
+    end
+  end
 
-      splits = Suffixer.new(total_parts).map do |suffix|
-        message_length_to_extract = 160 - suffix.length - 1
-        message_copy.slice!(0..message_length_to_extract) + suffix
-      end
+  def self.call(message)
+    new(message).call
+  end
 
-      return splits if message_copy.empty?
+  private
+
+  attr_reader :message
+
+  def message_fits_into_one_sms?
+    message.length <= MAX_SMS_LENGTH
+  end
+
+  def extract_message
+    lambda do |suffix, message_copy|
+      message_length_to_extract = MAX_SMS_LENGTH - suffix.length - 1
+      message_copy.slice!(0..message_length_to_extract) + suffix
     end
   end
 end
